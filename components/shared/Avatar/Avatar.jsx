a122abf5777054b4b96c45dfa4981a0aa0545688
cardfaux <cardfaux@tutanota.com>
2021-01-17 19:31:44 -0600

    added a few components for users and userlists and css modules for what I could

---
+++ components/shared/Avatar/Avatar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import styles from './Avatar.module.css';
+
+const Avatar = (props) => {
+  return (
+    // <div className={`avatar ${props.className}`} style={props.style}>
+    //   <img
+    //     src={props.image}
+    //     alt={props.alt}
+    //     style={{ width: props.width, height: props.width }}
+    //   />
+    // </div>
+
+    <div className={`${styles.avatar} ${props.className}`} style={props.style}>
+      <img
+        src={props.image}
+        alt={props.alt}
+        style={{ width: props.width, height: props.width }}
+      />
+    </div>
+  );
+};
+
+export default Avatar;
--- pages/users/index.jsx
+++ pages/users/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
-const index = () => {
-  return (
-    <div>
-      <h2>Users Works</h2>
-    </div>
-  );
+import UsersList from '../../components/user/UsersList/UsersList';
+
+const Users = () => {
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'James Hagood',
+      image:
+        'https://sparedshared16.files.wordpress.com/2018/02/hagood.png?w=175&h=277',
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
-export default index;
+export default Users;
